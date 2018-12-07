13ee4cc3178e46b5fbbe0b2245b3b5ed6edfb9c0
Alex Merenda <alex.merenda@impact.com>
2018-12-07 13:49:20 -0500

    updates path for python

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -7,5 +7,6 @@
 # Cache pip-installed packages to avoid re-downloading
 export ANSIBLE_NOCOWS=1
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+export INVENTORY_IP_TYPE=internal
 export DEFAULT_USER=alexmerenda
-export PATH=$PATH:/usr/local/bin:/usr/local/Cellar/coreutils/8.30/bin
+export PATH="/usr/local/opt/python/libexec/bin:/Users/alexmerenda/Library/Python/3.7/bin:/usr/local/bin:$PATH"
