e2e4151e781f2138f76ad1684d3482235f25cd26
Alex Merenda <amerenda89@gmail.com>
2019-07-02 17:53:05 -0400

    INTERNAL: work changes for macOS (#22)
    Adds misc scripts for my work
    Adds some servers to my ssh/config file
    Updates path and removes unused and expired github API token

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -9,5 +9,4 @@ export ANSIBLE_NOCOWS=1
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 export INVENTORY_IP_TYPE=internal
 export DEFAULT_USER=alexmerenda
-export PATH="/Users/alexmerenda/.bin:/usr/local/opt/python/libexec/bin:/Users/alexmerenda/Library/Python/3.7/bin:/usr/local/bin:$(brew --prefix openvpn)/sbin:$PATH"
-export HOMEBREW_GITHUB_API_TOKEN=303726bdbe81c21c613e332d8a1bde3c4ab804ab
+export PATH="/Users/alexmerenda/.bin:/usr/local/opt/python/libexec/bin:/Users/alexmerenda/Library/Python/3.7/bin:/usr/local/bin:/usr/local/opt/openvpn/sbin:$PATH"
