2017be0467bff8fbb6bdec57c823ab57383083ea
Alex Merenda <alex.merenda@impact.com>
2019-07-17 15:58:46 -0400

    removes git ignore

---
+++ zsh/plugins.zsh
@@ -0,0 +1,5 @@
+antigen bundle denysdovhan/spaceship-prompt
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
+antigen apply
