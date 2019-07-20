8f8eba064dcd59416847d97adb662611934342d6
Alex Merenda <amerenda89@gmail.com>
2019-07-20 10:11:12 -0400

    Revert "removes git ignore"
    This reverts commit 928dbb7c8552bed7e1baf39b10ddaeb32388d77a.

--- zsh/plugins.zsh
+++
@@ -1,5 +0,0 @@
-antigen bundle denysdovhan/spaceship-prompt
-antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-history-substring-search
-antigen apply
