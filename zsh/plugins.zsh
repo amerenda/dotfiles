0fb8cf0d5d9f01b9cb9e83e32d284d3a487e81ed
Alex Merenda <alex.merenda@impact.com>
2019-08-14 23:39:23 -0400

    updates dotfiles

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,6 @@
 antigen bundle denysdovhan/spaceship-prompt
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle joel-porquet/zsh-dircolors-solarized.git
 antigen bundle zsh-users/zsh-history-substring-search
 antigen apply
