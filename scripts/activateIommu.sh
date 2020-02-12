062e7bfecc9705b22a3fc14037b7f0f815425b20
Alex Merenda <alex.merenda@impact.com>
2020-02-12 10:39:02 -0500

    adds vm scripts

--- scripts/activateIommu.sh
+++ scripts/activateIommu.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo '0000:01:00.2' > /sys/bus/pci/drivers/xhci_hcd/unbind
 echo '0000:01:00.2' > /sys/bus/pci/drivers/vfio-pci/bind
+#echo '0000:03:00.0' > /sys/bus/pci/drivers/xhci_hcd/unbind
+#echo '0000:03:00.0' > /sys/bus/pci/drivers/vfio-pci/bind
