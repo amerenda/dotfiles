21af83b16c1eda9c0001e953cf75b06ef146087b
amerenda <alexm@moove.ai>
2023-06-27 14:30:15 -0600

    terraform

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -13,6 +13,10 @@ export PATH="$HOME/.local/bin:$PATH"
 # Google variables
 export GOOGLE_APPLICATION_CREDENTIALS=~/.gcloud_creds/terraform-serviceaccount.json
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+
+# Terraform
+export TF_LOG_PATH=~/tmp/terraform-debug.log
+
 # Evals
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "$(direnv hook zsh)"
