151c849c6f6b9848a3e0da77f8ecd7d1cf61310c
amerenda <alexm@moove.ai>
2023-10-07 15:54:54 -0400

    sets TMPDIR to /tmp on linux

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -4,6 +4,7 @@ export GOROOT=/usr/lib/go
 export GOBIN=$HOME/projects/go/bin
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+export TMPDIR=/tmp
 
 # There is some kind of bug where capslock behaves as both escape and capslock, this fixes it for gnome
 # Check if the XDG_CURRENT_DESKTOP environment variable is set
