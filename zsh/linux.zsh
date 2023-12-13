7e4131951c02e6c3533e0d66b344a3fad3ad44b2
amerenda <alexm@moove.ai>
2023-12-13 11:51:41 -0500

    removes gsettings config in favor of a more generic approach

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -6,16 +6,16 @@ export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 export TMPDIR=/tmp
 
-# There is some kind of bug where capslock behaves as both escape and capslock, this fixes it for gnome
-# Check if the XDG_CURRENT_DESKTOP environment variable is set
-if [ -n "$XDG_CURRENT_DESKTOP" ]; then
-    # Check if the value contains "GNOME" (case insensitive)
-    if [[ "$XDG_CURRENT_DESKTOP" =~ [Gg][Nn][Oo][Mm][Ee] ]]; then
-        gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
-    else
-        exit 0
-    fi
-else
-    exit 0
-fi
+## There is some kind of bug where capslock behaves as both escape and capslock, this fixes it for gnome
+## Check if the XDG_CURRENT_DESKTOP environment variable is set
+#if [ -n "$XDG_CURRENT_DESKTOP" ]; then
+#    # Check if the value contains "GNOME" (case insensitive)
+#    if [[ "$XDG_CURRENT_DESKTOP" =~ [Gg][Nn][Oo][Mm][Ee] ]]; then
+#        gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+#    else
+#        exit 0
+#    fi
+#else
+#    exit 0
+#fi
 
